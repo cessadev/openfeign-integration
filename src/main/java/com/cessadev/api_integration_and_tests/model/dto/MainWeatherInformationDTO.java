08d3ee1a65a2e846cd060667be1f94fcecd5c091
cessadev <cesarurb.dev@gmail.com>
2024-09-29 13:25:47 -0500

    Added model

---
+++ src/main/java/com/cessadev/api_integration_and_tests/model/dto/MainWeatherInformationDTO.java
@@ -0,0 +1,6 @@
+package com.cessadev.api_integration_and_tests.model.dto;
+
+public record MainWeatherInformationDTO() {
+    private static double temp;
+    private static int humidity;
+}
---
+++ src/main/java/com/cessadev/api_integration_and_tests/model/dto/WeatherResponseDTO.java
@@ -0,0 +1,6 @@
+package com.cessadev.api_integration_and_tests.model.dto;
+
+public record WeatherResponseDTO() {
+    private static String name;
+    private static MainWeatherInformationDTO main;
+}
